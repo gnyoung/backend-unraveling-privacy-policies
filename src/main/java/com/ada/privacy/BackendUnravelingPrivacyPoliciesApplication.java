f94baa7f7ea8b5ae276634abf08c261758584b0a
Gabby <gabrielleyoung10@gmail.com>
2023-08-02 15:27:47 -0500

    Add project files

---
+++ src/main/java/com/ada/privacy/BackendUnravelingPrivacyPoliciesApplication.java
@@ -0,0 +1,13 @@
+package com.ada.privacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUnravelingPrivacyPoliciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUnravelingPrivacyPoliciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ada/privacy/BackendUnravelingPrivacyPoliciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.privacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUnravelingPrivacyPoliciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
